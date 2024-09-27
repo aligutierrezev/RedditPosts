3b362ec6d9a7bc1381a5861a32348d5cda8fd418
Ali Gutierrez <roberto.gutierrez@evite.com>
2024-09-26 19:53:02 -0600

    Initial Commit

---
+++ RedditPosts/RedditPosts/RedditPostsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedditPostsApp.swift
+//  RedditPosts
+//
+//  Created by Ali Gutierrez on 26/09/24.
+//
+
+import SwiftUI
+
+@main
+struct RedditPostsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
